7636fab7783bc2dca102bb51a6b0d9c684a5ce8e
ddecataldo <davidedecataldo92@gmail.com>
2021-10-27 15:50:31 +0200

    Upload JavaScript

---
+++ scripts.js
@@ -0,0 +1,44 @@
+/* Un alert() espone 5 numeri generati casualmente.
+Da li parte un timer di 30 secondi.
+Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati. */
+
+// Creare 5 numeri random
+// Aspettare 30 secondi e chiedere all'utente 5 numeri
+// Verificare se tra i numeri inseriti dall'utente c'è qualcuno che corrisponde ai numeri random generati
+// Comunicare all'utente quanti e quali numeri sono stati indovinati
+
+// Creo una funzione che mi permette di generare dei numeri random impostando un valore minimo e un valore massimo
+function generateRandomNum(minNumber, maxNumber) {
+    const numRandom = Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
+    return numRandom;
+}
+
+// Dichiaro un array vuoto dove inserire i 5 numeri random
+const numeriRandom = [];
+// Dichiaro un array vuoto dove inserire i 5 numeri random inseriti dall'utente
+const numeriRandomUtente = [];
+
+// Ripeto il ciclo 5 volte, ed ogni volta pusho un numero random nell'array numeriRandom
+for (let i = 0; i < 5; i++) {
+    numeriRandom.push(generateRandomNum(1, 100))
+}
+// Stampo i numeri random
+alert(numeriRandom);
+
+setTimeout(arrayNumeriRandomUtente, 30000);
+
+
+function arrayNumeriRandomUtente(){
+    let numeriUtente;
+    do {
+        numeriUtente = prompt("Inserisci un numero");
+        console.log(numeriUtente)
+        numeriRandomUtente.push(numeriUtente);
+
+    } while(!isNaN(parseInt(numeriUtente)) && numeriRandomUtente.length < 5)
+
+    return numeriUtente;
+}
+
+console.log(arrayNumeriRandomUtente());
